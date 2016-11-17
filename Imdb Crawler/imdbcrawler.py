19040890a45e64b85fc56d34ccc9d12e0f50f54b
Stepan Ulyanin <steve.usuck@gmail.com>
2016-11-17 00:49:54 -0600

    urllib2.httplib.BadStatusLine exception added

--- Imdb Crawler/imdbcrawler.py
+++ Imdb Crawler/imdbcrawler.py
@@ -19,44 +19,51 @@ class ImdbCrawler(object):
         # request for pages in the list
         for i in range(1, 8001, 100):
 
-            print("Requesting: {}".format(i))
 
-            request = urllib2.urlopen('http://www.imdb.com/list/ls057823854/?start=' + str(i) +
-                                  '&view=detail&sort=listorian:asc')
+            try:
 
-            # parse the response
-            soup = BeautifulSoup(request.read(), 'html.parser')
+                print("Requesting: {}".format(i))
 
-            # get all the info classes
-            info = soup.find_all("div", attrs={"class": "info"})
+                request = urllib2.urlopen('http://www.imdb.com/list/ls057823854/?start=' + str(i) +
+                                      '&view=detail&sort=listorian:asc')
 
-            # extract the data from the soup
-            for index, _ in enumerate(info):
+                # parse the response
+                soup = BeautifulSoup(request.read(), 'html.parser')
 
-                try:
-                    # iterate through the movies and gather data
-                    name = info[index].find_next("a").string
-                    year = info[index].find_next("span").string
-                    description = info[index].find_all("div", attrs={"class": "item_description"})[0].text
-                    score = info[index].find_all("span", attrs={"class": "rating-rating"})[0].text
-                    director = info[index].find_all("div", attrs={"class": "secondary"})[0].text
-                    stars = info[index].find_all("div", attrs={"class": "secondary"})[1].text
+                # get all the info classes
+                info = soup.find_all("div", attrs={"class": "info"})
 
-                # some scores don't have scores
-                except IndexError:
-                    score = "N/A"
+                # extract the data from the soup
+                for index, _ in enumerate(info):
 
-                # define the dictionary
-                self.movie = {"name": name, "year": year, "director": director, "stars": stars, "score": score,
-                                "description": description}
+                    try:
+                        # iterate through the movies and gather data
+                        name = info[index].find_next("a").string
+                        year = info[index].find_next("span").string
+                        description = info[index].find_all("div", attrs={"class": "item_description"})[0].text
+                        score = info[index].find_all("span", attrs={"class": "rating-rating"})[0].text
+                        director = info[index].find_all("div", attrs={"class": "secondary"})[0].text
+                        stars = info[index].find_all("div", attrs={"class": "secondary"})[1].text
 
-                # append the dictionaries to the full list of the movies
-                self.movies_info.append(self.movie)
+                    # some scores don't have scores
+                    except IndexError:
+                        score = "N/A"
 
-            # delete the urllib and soup objects to reset
-            del request
-            del soup
-            del info
+                    # define the dictionary
+                    self.movie = {"name": name, "year": year, "director": director, "stars": stars, "score": score,
+                                    "description": description}
+
+                    # append the dictionaries to the full list of the movies
+                    self.movies_info.append(self.movie)
+
+                # delete the urllib and soup objects to reset
+                del request
+                del soup
+                del info
+
+            except urllib2.httplib.BadStatusLine:
+                print("Restarting the Crawl() due to: {}".format(urllib2.httplib.BadStatusLine.message))
+                self.crawl()
 
     def write_to_console(self):
         """
@@ -78,10 +85,10 @@ class ImdbCrawler(object):
 
         # break the data into the lists
         for movie in self.movies_info:
-            names.append(str(movie['name']))
-            directors.append(str(movie['director'].replace("Director: ", "")))
-            years.append(str(movie['year']))
-            summaries.append(str(movie['description']))
+            names.append(str(movie['name'].encode('ascii', 'ignore')))
+            directors.append(str(movie['director'].replace("Director: ", "").encode('ascii', 'ignore')))
+            years.append(str(movie['year'].encode('ascii', 'ignore')))
+            summaries.append(str(movie['description'].encode('ascii', 'ignore')))
             stars.append(str(movie['stars'].replace("Stars: ", "").encode('ascii', 'ignore')))
 
         # create the SFrame object
