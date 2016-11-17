8147e07690b45aa5d4624a9e28b9c4fef1726d22
Stepan Ulyanin <steve.usuck@gmail.com>
2016-11-16 21:16:37 -0600

    imdb crawler init

--- Imdb Crawler/imdbcrawler.py
+++ Imdb Crawler/imdbcrawler.py
@@ -1,5 +1,37 @@
-def main():
+from bs4 import BeautifulSoup
+import re
+import urllib.request as url
+
+
+class ImdbCrawler(object):
+
     pass
+
+
+def main():
+
+    movies_info = list()
+
+    # for i in range(1, 301, 100):
+    #     print(i)
+
+    for i in range(1, 101, 100):
+        request = url.urlopen('http://www.imdb.com/list/ls057823854/?start=' + str(i) +
+                              '&view=detail&sort=listorian:asc')
+
+        print('http://www.imdb.com/list/ls057823854/?start=' + str(i) + '&view=detail&sort=listorian:asc')
+
+        soup = BeautifulSoup(request.read(), 'html.parser')
+        info = soup.find_all("div", attrs={"class": "info"})
+
+        # 0 is for the iterator over 100 movies in the loop
+        name = info[0].find_next("a").string
+        year = info[0].find_next("span").string
+        description = info[0].find_all("div", attrs={"class": "item_description"})[0].text
+        score = info[0].find_all("span", attrs={"class": "rating-rating"})[0].text
+
+        print(info[0])
+
 
 if __name__ == "__main__":
     main()
