26a1eb3c686e58d198c5c11080111794187fd2c4
Stepan Ulyanin <steve.usuck@gmail.com>
2016-11-16 23:42:35 -0600

    crawler class constructed

--- Imdb Crawler/imdbcrawler.py
+++ Imdb Crawler/imdbcrawler.py
@@ -1,37 +1,104 @@
 from bs4 import BeautifulSoup
-import re
-import urllib.request as url
+import urllib2
+import graphlab
+from graphlab import SArray
 
 
 class ImdbCrawler(object):
+    """
+        Crawler class
+    """
+    def __init__(self):
+        self.movie = dict()
+        self.movies_info = list()
 
-    pass
+    def crawl(self):
+        """
+            Requests data from imdb list and stores in self.movie_info
+        """
+        # request for pages in the list
+        for i in range(1, 8001, 100):
+
+            print("Requesting: {}".format(i))
+
+            request = urllib2.urlopen('http://www.imdb.com/list/ls057823854/?start=' + str(i) +
+                                  '&view=detail&sort=listorian:asc')
+
+            # parse the response
+            soup = BeautifulSoup(request.read(), 'html.parser')
+
+            # get all the info classes
+            info = soup.find_all("div", attrs={"class": "info"})
+
+            # extract the data from the soup
+            for index, _ in enumerate(info):
+
+                try:
+                    # iterate through the movies and gather data
+                    name = info[index].find_next("a").string
+                    year = info[index].find_next("span").string
+                    description = info[index].find_all("div", attrs={"class": "item_description"})[0].text
+                    score = info[index].find_all("span", attrs={"class": "rating-rating"})[0].text
+                    director = info[index].find_all("div", attrs={"class": "secondary"})[0].text
+                    stars = info[index].find_all("div", attrs={"class": "secondary"})[1].text
+
+                # some scores don't have scores
+                except IndexError:
+                    score = "N/A"
+
+                # define the dictionary
+                self.movie = {"name": name, "year": year, "director": director, "stars": stars, "score": score,
+                                "description": description}
+
+                # append the dictionaries to the full list of the movies
+                self.movies_info.append(self.movie)
+
+            # delete the urllib and soup objects to reset
+            del request
+            del soup
+            del info
+
+    def write_to_console(self):
+        """
+            Prints the movie list
+        """
+        for i, movie in enumerate(self.movies_info):
+            print(i, movie)
+
+    def write_to_frame(self):
+        """
+            Creates an SFrame object and exports it for use in a different framework
+        """
+        # define the lists
+        names = list()
+        directors = list()
+        years = list()
+        summaries = list()
+        stars = list()
+
+        # break the data into the lists
+        for movie in self.movies_info:
+            names.append(str(movie['name']))
+            directors.append(str(movie['director'].replace("Director: ", "")))
+            years.append(str(movie['year']))
+            summaries.append(str(movie['description']))
+            stars.append(str(movie['stars'].replace("Stars: ", "").encode('ascii', 'ignore')))
+
+        # create the SFrame object
+        frame = graphlab.SFrame({'name': SArray(names), 'description': summaries,
+                                 'director': directors, 'year': years, 'stars': stars})
+
+        # save the SFrame
+        frame.save('Imdb Data')
+
+        print("Done Saving to SFrame")
 
 
 def main():
 
-    movies_info = list()
-
-    # for i in range(1, 301, 100):
-    #     print(i)
-
-    for i in range(1, 101, 100):
-        request = url.urlopen('http://www.imdb.com/list/ls057823854/?start=' + str(i) +
-                              '&view=detail&sort=listorian:asc')
-
-        print('http://www.imdb.com/list/ls057823854/?start=' + str(i) + '&view=detail&sort=listorian:asc')
-
-        soup = BeautifulSoup(request.read(), 'html.parser')
-        info = soup.find_all("div", attrs={"class": "info"})
-
-        # 0 is for the iterator over 100 movies in the loop
-        name = info[0].find_next("a").string
-        year = info[0].find_next("span").string
-        description = info[0].find_all("div", attrs={"class": "item_description"})[0].text
-        score = info[0].find_all("span", attrs={"class": "rating-rating"})[0].text
-
-        print(info[0])
-
+    crawler = ImdbCrawler()
+    crawler.crawl()
+    crawler.write_to_frame()
 
 if __name__ == "__main__":
     main()
