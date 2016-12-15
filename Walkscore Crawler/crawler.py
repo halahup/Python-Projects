051b2333239ed18e4a0d37874f76854d086aa07d
Stepan Ulyanin <steve.usuck@gmail.com>
2016-12-14 19:00:00 -0600

    exceptions added

--- Walkscore Crawler/crawler.py
+++ Walkscore Crawler/crawler.py
@@ -6,11 +6,13 @@ import sys
 
 
 class Crawler:
-
+    """
+        Crawles out to walkscore.com and scraps the data about complexes
+    """
     def __init__(self, city, state):
         self.city = city
         self.state = state
-        self.url = "https://www.walkscore.com/apartments/featured/" + state + "/" + city
+        self.url = "https://www.walkscore.com/apartments/featured/" + state.upper() + "/" + city
         self.soup = None
         self.data = None
 
@@ -27,70 +29,98 @@ class Crawler:
 
     def crawl(self):
 
+        # TODO: scrap the number of bedrooms
+
         # header
-        print("city: {}, state: {}".format(self.city.upper(), self.state))
+        print("city: {}, state: {}".format(self.city.upper(), self.state.upper()))
 
         self.data = list()
 
-        for page in range(1, 32):
+        for page in range(1, 1000):
 
-            # request the page
-            if page == 1:
-                request = urllib.request.urlopen(self.url)
-
-            else:
-                request = urllib.request.urlopen(self.url + "/p" + str(page))
-
-            print("PROCESSING PAGE ", page)
-
-            # parse the page
-            self.soup = BeautifulSoup(request.read(), 'html.parser')
-
-            # listings on the page
-            listings = self.soup.find_all("div", attrs={"class": "listing-wrap"})
-
-            # print(listings)
-
-            for listing in listings:
-
-                # price
-                price_html = listing.find_all("div", attrs={"class": "price"})
-
-                # if "Contact" instead of price - set price as n/a
-                if "Contact" in price_html[0].text:
-                    price = "N/A"
-
-                elif "from" not in price_html[0].text and "Contact" not in price_html[0].text:
-                    price = listing.find_all("div", attrs={"class": "price"})[0].text.lstrip().rstrip()
+            try:
+                # request the page
+                if page == 1:
+                    request = urllib.request.urlopen(self.url)
 
                 else:
-                    price = listing.find_all("div", attrs={"class": "price"})[0].text[7:].rstrip()
+                    request = urllib.request.urlopen(self.url + "/p" + str(page))
 
-                # names
-                try:
-                    name = listing.find_all("div", attrs={"class": "name"})[0].text.lstrip().rstrip()
-                except IndexError:
-                    name = "N/A"
+                print("PROCESSING PAGE ", page)
 
-                # walkscore
-                try:
-                    walkscore = listing.find_all("div", attrs={"class": "walk-score"})[0].text[10:].lstrip().rstrip()
-                except IndexError:
-                    walkscore = "N/A"
+                # parse the page
+                self.soup = BeautifulSoup(request.read(), 'html.parser')
 
-                entry = {"name": name, "price": price, "walkscore": walkscore}
+                # listings on the page
+                listings = self.soup.find_all("div", attrs={"class": "listing-wrap"})
 
-                self.data.append(entry)
+                assert len(listings) != 0
+
+                # print(listings)
+
+                for listing in listings:
+
+                    # price
+                    price_html = listing.find_all("div", attrs={"class": "price"})
+
+                    # if "Contact" instead of price - set price as n/a
+                    if "Contact" in price_html[0].text:
+                        price = "N/A"
+
+                    elif "from" not in price_html[0].text and "Contact" not in price_html[0].text:
+                        price = listing.find_all("div", attrs={"class": "price"})[0].text.lstrip().rstrip()
+
+                    else:
+                        price = listing.find_all("div", attrs={"class": "price"})[0].text[7:].rstrip()
+
+                    # names
+                    try:
+                        name = listing.find_all("div", attrs={"class": "name"})[0].text.lstrip().rstrip()
+                    except IndexError:
+                        name = "N/A"
+
+                    # walkscore
+                    try:
+                        walkscore = listing.find_all("div", attrs={"class": "walk-score"})[0].text[10:].lstrip().rstrip()
+                    except IndexError:
+                        walkscore = "N/A"
+
+                    # beds
+                    try:
+                        beds = listing.find_all("div", attrs={"class": "beds"})[0].text.lstrip().rstrip()
+
+                    except IndexError:
+                        print("Error")
+
+                    entry = {"name": name, "price": price, "walkscore": walkscore, "beds": beds}
+
+                    self.data.append(entry)
+
+            # reached the end of the pages
+            except AssertionError:
+                print("{} pages has been scraped for {}".format(page, self.city.upper()))
+
+                # save the CSV
+                self.save_csv()
+                return
+
+            except urllib.error.HTTPError:
+                print("No such city on walkscore.com or city/state is misspelled")
+                return
 
     def save_csv(self):
-        with open("{}_{}.csv".format(self.city.upper(), self.state), "w", newline='') as csvfile:
+        """
+            Writes the data into the csv
+        """
+        with open("{}_{}.csv".format(self.city.upper(), self.state.upper()), "w", newline='') as csvfile:
             writer = csv.writer(csvfile)
-
+            writer.writerow(["Name", "Price", "Walkscore", "Beds"])
             for entry in self.data:
-                data = [entry["name"], entry["price"], entry["walkscore"]]
+                data = [entry["name"], entry["price"], entry["walkscore"], entry["beds"]]
                 writer.writerow(data)
 
         print("Data written to CSV")
+
 
 def main():
 
@@ -103,7 +133,6 @@ def main():
 
     # launch
     crawler.crawl()
-    crawler.save_csv()
 
     # Crawler.get_list_of_cities()
 
