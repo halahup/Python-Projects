d636d2d5e2fbebb4bc0dbbda88b47deb9b84bc3b
Stepan Ulyanin <steve.usuck@gmail.com>
2017-07-15 11:50:39 -0400

    flask added

--- Lighthouse/flask_face_recog.py
+++ Lighthouse/flask_face_recog.py
@@ -4,79 +4,6 @@ import numpy as np
 import dlib
 import sys
 
-
-def main(test_image=None):
-    print('Got here')
-    # read the test_image as an argument
-    if test_image is None:
-        test_image = r(sys.argv[1], mode='RGB')
-
-        print("Image: {}".format(sys.argv[1]))
-
-    # 1. obtain the face encodings
-    test_image_face_encodings = face_encodings(test_image)
-    print("Found {} faces".format(len(test_image_face_encodings)))
-
-    # data entries are going here
-    names = [
-        "Dan Sharp",
-        "James Arnoldi",
-        "Jason Charles",
-        "John Simmons",
-        "Alan Spohn",
-        "Parul Luthra",
-        "Mike Stedman",
-        "Suman Biswas",
-        "Tyler Zupan"
-    ]
-
-    # read the known images - data warehouse
-    parul = r("./Mugshots/Parul Luthra.jpg", mode='RGB')
-    alan = r("./Mugshots/Alan Spohn.jpg", mode='RGB')
-    dan = r("./Mugshots/Dan Sharp.jpg", mode='RGB')
-    james = r("./Mugshots/James Arnoldi.jpg", mode='RGB')
-    jason = r("./Mugshots/Jason Charles.jpg", mode='RGB')
-    john = r("./Mugshots/John Simmons.jpg", mode='RGB')
-    mike = r("./Mugshots/Mike Stedman.jpg", mode='RGB')
-    suman = r("./Mugshots/Suman Biswas.jpg", mode='RGB')
-    tyler = r("./Mugshots/Tyler Zupan.jpg", mode='RGB')
-
-    # encode the images
-    parul_enc = face_encodings(parul)[0]
-    alan_enc = face_encodings(alan)[0]
-    dan_enc = face_encodings(dan)[0]
-    james_enc = face_encodings(james)[0]
-    jason_enc = face_encodings(jason)[0]
-    john_enc = face_encodings(john)[0]
-    mike_enc = face_encodings(mike)[0]
-    suman_enc = face_encodings(suman)[0]
-    tyler_enc = face_encodings(tyler)[0]
-
-    # database of encoded mugshots
-    known_images = [
-        dan_enc,
-        james_enc,
-        jason_enc,
-        john_enc,
-        alan_enc,
-        parul_enc,
-        mike_enc,
-        suman_enc,
-        tyler_enc
-    ]
-
-    # for every face found on the photo, run the comparison
-    for i, enc in enumerate(test_image_face_encodings):
-
-        results = compare_faces(known_images, enc)
-
-        if True in results:
-            print("Match Found at index: {}".format(i))
-            print(names[results.index(True)])
-
-    return 0
-
-
 # load the models -- ??? PRETRAINED ???
 face_recognition_model_loc = "./Models/dlib_face_recognition_resnet_model_v1.dat"
 face_encoder = dlib.face_recognition_model_v1(face_recognition_model_loc)
@@ -226,5 +153,81 @@ def compare_faces(known_face_encodings, face_encoding_to_check, tolerance=0.6):
     :return: A list of True/False values indicating which known_face_encodings match the face encoding to check
     """
     return list(face_distance(known_face_encodings, face_encoding_to_check) <= tolerance)
+
+
+def main(raw_image=None):
+
+    # read the test_image as an argument
+    if raw_image is None:
+        test_image = r(sys.argv[1], mode='RGB')
+
+        print("Image: {}".format(sys.argv[1]))
+    else:
+        test_image = r(raw_image, mode='RGB')
+
+    # 1. obtain the face encodings
+    test_image_face_encodings = face_encodings(test_image)
+    print("Found {} faces".format(len(test_image_face_encodings)))
+
+    # data entries are going here
+    names = [
+        "Dan Sharp",
+        "James Arnoldi",
+        "Jason Charles",
+        "John Simmons",
+        "Alan Spohn",
+        "Parul Luthra",
+        "Mike Stedman",
+        "Suman Biswas",
+        "Tyler Zupan"
+    ]
+
+    # read the known images - data warehouse
+    parul = r("./Mugshots/Parul Luthra.jpg", mode='RGB')
+    alan = r("./Mugshots/Alan Spohn.jpg", mode='RGB')
+    dan = r("./Mugshots/Dan Sharp.jpg", mode='RGB')
+    james = r("./Mugshots/James Arnoldi.jpg", mode='RGB')
+    jason = r("./Mugshots/Jason Charles.jpg", mode='RGB')
+    john = r("./Mugshots/John Simmons.jpg", mode='RGB')
+    mike = r("./Mugshots/Mike Stedman.jpg", mode='RGB')
+    suman = r("./Mugshots/Suman Biswas.jpg", mode='RGB')
+    tyler = r("./Mugshots/Tyler Zupan.jpg", mode='RGB')
+
+    # encode the images
+    parul_enc = face_encodings(parul)[0]
+    alan_enc = face_encodings(alan)[0]
+    dan_enc = face_encodings(dan)[0]
+    james_enc = face_encodings(james)[0]
+    jason_enc = face_encodings(jason)[0]
+    john_enc = face_encodings(john)[0]
+    mike_enc = face_encodings(mike)[0]
+    suman_enc = face_encodings(suman)[0]
+    tyler_enc = face_encodings(tyler)[0]
+
+    # database of encoded mugshots
+    known_images = [
+        dan_enc,
+        james_enc,
+        jason_enc,
+        john_enc,
+        alan_enc,
+        parul_enc,
+        mike_enc,
+        suman_enc,
+        tyler_enc
+    ]
+
+    # for every face found on the photo, run the comparison
+    for i, enc in enumerate(test_image_face_encodings):
+
+        results = compare_faces(known_images, enc)
+
+        if True in results:
+            print("Match Found at index: {}".format(i))
+            print(names[results.index(True)])
+
+    return 0
+
+
 
 
